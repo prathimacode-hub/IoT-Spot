8d31d57672a3fac4a7cd3284d2593008ff5eb0f5
talkshrey <talkshrey@gmail.com>
2021-12-13 11:52:08 +0530

    Comments added in the code

--- Arduino/LCD Welcome Message/lcd_welcome_message_using_arduino.ino
+++ Arduino/LCD Welcome Message/lcd_welcome_message_using_arduino.ino
@@ -6,8 +6,12 @@ String msg2 = "IoT-Spot";
 int size = msg2.length();
 
 void setup() {
+
+  // set pin mode to output
   pinMode(buzzerPin, OUTPUT);
   lcd.begin(16, 2);
+
+  // adding a sound before the message is display
   tone(buzzerPin, 1000);  
   delay(50);
   noTone(buzzerPin);
@@ -15,9 +19,10 @@ void setup() {
 }
 
 void loop() {
+
+  // setting lcd to initial position
   lcd.setCursor(0, 0);
   lcd.print(msg1);
   lcd.setCursor(16-size, 1);
   lcd.print(msg2);
-  
 }
