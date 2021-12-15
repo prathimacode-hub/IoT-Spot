6e90be8b5f32bd3a1d84f71c69c6f3af2d46e9d9
Baibhav <baibhavmishra3211@gmail.com>
2021-12-15 08:53:16 +0530

    added comments in the code

--- Arduino/Speeding Motor Using Arduino/speeding_motor_using_arduino1.ino
+++ Arduino/Speeding Motor Using Arduino/speeding_motor_using_arduino1.ino
@@ -1,5 +1,8 @@
+// Setting the pin configuration of motor and potentiometer
 int potentioPin = A0;
 int motorPin = 9;
+
+// Setting the initial values of motor and potentiometer
 int potentioValue = 0;
 int motorValue = 0;
 
@@ -8,12 +11,12 @@ void setup() {
 }
 
 void loop() {
- potentioValue = analogRead(potentioPin);  
- motorValue = map(potentioValue, 0, 1023, 0, 255);
- analogWrite(motorPin, motorValue);  
+ potentioValue = analogRead(potentioPin);      // Reading the input of potentiometer (0-1023)
+ motorValue = map(potentioValue, 0, 1023, 0, 255);  // Scaling the motor values to range (0-255)
+ analogWrite(motorPin, motorValue);              // Output the PWM signal to motorpin
  Serial.print("potentiometer = " );     
- Serial.print(potentioValue);
+ Serial.print(potentioValue);                // Printing the potentiometer value on serial monitor
  Serial.print(" motor value = ");
- Serial.println(motorValue);
- delay(2);    
+ Serial.println(motorValue);                 // Printing the motor value on serial monitor
+ delay(2);                                   // Time delay to overcome motor inertia
 }
