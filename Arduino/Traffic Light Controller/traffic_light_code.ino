658f0abfc67d46ae6b9f93c0a73f92fec375bbd5
ritikalohia <56797604+ritikalohia@users.noreply.github.com>
2021-12-21 15:58:48 +0530

    Update traffic_light_code.ino

--- Arduino/Traffic Light Controller/traffic_light_code.ino
+++ Arduino/Traffic Light Controller/traffic_light_code.ino
@@ -22,14 +22,14 @@ void loop()
   digitalWrite(8,0);
   digitalWrite(9,0);
   digitalWrite(5,0);
-  delay(5000);
+  delay(1000);
  
   digitalWrite(3,1); //enables the 1st set of signals
   digitalWrite(6,0);
   digitalWrite(2,0); 
   digitalWrite(7,1);
   digitalWrite(10,1);
-  delay(1000);
+  delay(5000);
  
   digitalWrite(4,1); //enables the yellow lights
   digitalWrite(5,0);
@@ -40,7 +40,7 @@ void loop()
   digitalWrite(8,1);
   digitalWrite(9,0);
   digitalWrite(7,1);
-  delay(5000);
+  delay(1000);
  
   digitalWrite(9,1); //enables the 2nd set of signals
   digitalWrite(7,1);  
@@ -48,7 +48,7 @@ void loop()
   digitalWrite(10,0);
   digitalWrite(5,0);  
   digitalWrite(4,1);
-  delay(1000); 
+  delay(5000); 
    
   digitalWrite(8,0);  //enables the yellow lights
   digitalWrite(4,1); 
@@ -59,12 +59,12 @@ void loop()
   digitalWrite(6,0);
   digitalWrite(9,0);
   digitalWrite(10,1);
-  delay(5000);
+  delay(1000);
  
   digitalWrite(6,1); //enables the 3rd set of signals
   digitalWrite(10,1); 
   digitalWrite(7,0);
   digitalWrite(8,0); 
   digitalWrite(4,1);
-  delay(1000);   
+  delay(5000);   
 }
