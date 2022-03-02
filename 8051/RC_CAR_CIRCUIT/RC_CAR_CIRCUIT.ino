5f1dfe67c098de8118e9000211babd21f14f3225
Rahul Kashyap <rk.20u10641@btech.nitdgp.ac.in>
2022-03-02 16:35:29 +0530

    Changes will rotates to will rotate

--- 8051/RC_CAR_CIRCUIT/RC_CAR_CIRCUIT.ino
+++ 8051/RC_CAR_CIRCUIT/RC_CAR_CIRCUIT.ino
@@ -1,10 +1,10 @@
 //In this Project we are going to make RC Car Circuit.
 // For this the components used will be 2 DC Motor, Arduino UNO R3, BreadBoard, L293D Motor Driver.
 /* If we give the input:
-   F- Forward, Both Motor will rotates in clockwise direction.
-   B- Backward, Both Motor will rotates in anti clockwise direction.
-   L- Left, Left motor will rotates in anticlockwise dirction and Right motor will rotates in clockwise direction.
-   R- Right, Right motor will rotates in anticlockwise direction and Left motor will rotates in clockwise dirction
+   F- Forward, Both Motor will rotate in clockwise direction.
+   B- Backward, Both Motor will rotate in anti clockwise direction.
+   L- Left, Left motor will rotate in anticlockwise dirction and Right motor will rotate in clockwise direction.
+   R- Right, Right motor will rotate in anticlockwise direction and Left motor will rotate in clockwise dirction
    S- Stop, Both motor will stop rotating.*/
 // Input1 of L239D -- 6
 // Input2 of L239D -- 7
