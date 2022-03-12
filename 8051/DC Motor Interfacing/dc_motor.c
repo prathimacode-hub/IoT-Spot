684353b1515e62ca6fd85979ea6ab20d301cc1f1
nitish <nitish@localhost.localdomain>
2022-03-12 17:44:19 +0530

    added comments

--- 8051/DC Motor Interfacing/dc_motor.c
+++ 8051/DC Motor Interfacing/dc_motor.c
@@ -6,41 +6,41 @@ sbit switch3 = P1^2;//clockwise rotation for left motor only
 sbit switch4 = P1^3;//anticlockwise rotation for both motor
 sbit switch5 = P1^4;//stop rotation (made it always one so that if all switch is off then motor will be in off condition else in on condition)
 
-void delay(void)
+void delay(void)//delay function
 {
 	unsigned int i;
 	for(i=0;i<0xFF;i++);
 }
 void main()
 {
-	P1 = 0x00;
+	P1 = 0x00;//initial values
 	P2 = 0x00;
 	
 	while(1)
 	{
 		if(switch1 == 1)
 		{
-			P2 = 0x09;
+			P2 = 0x09;// leads to movement in clockwise direction
 		}
 		
 		if(switch2 == 1)
 		{
-			P2 = 0x04;
+			P2 = 0x04;//right dc motor rotates in clockwise direction
 		}
 		
 		if(switch3 == 1)
 		{
-			P2 = 0x01;
+			P2 = 0x01;//left dc motor rotates in clockwise direction
 		}
 		
 		if(switch4 == 1)
 		{
-			P2 = 0x06;
+			P2 = 0x06;//rotates both dc motor in anti clockwise direction
 		}
 		
 		if(switch5 == 1)
 		{
-			P2 = 0x00;
+			P2 = 0x00;//stops both dc motor
 		}
 	}
 }
