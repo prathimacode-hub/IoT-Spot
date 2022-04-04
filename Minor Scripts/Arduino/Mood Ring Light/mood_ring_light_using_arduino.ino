7f91806abbc55cde75dc3b6b0af84c901b3e3f8c
Shreya Ghosh <72400676+shreya024@users.noreply.github.com>
2022-04-04 16:03:47 +0530

    Mood Ring Light Added

---
+++ Minor Scripts/Arduino/Mood Ring Light/mood_ring_light_using_arduino.ino
@@ -0,0 +1,72 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define PIN 6
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup()
+{
+
+    if (F_CPU == 16000000)
+        clock_prescale_set(clock_div_1);
+
+    strip.begin();
+    strip.show();
+}
+
+void loop()
+{
+
+    rainbow(20);
+    rainbowCycle(20);
+}
+
+void rainbow(uint8_t wait)
+{
+    uint16_t i, j;
+
+    for (j = 0; j < 256; j++)
+    {
+        for (i = 0; i < strip.numPixels(); i++)
+        {
+            strip.setPixelColor(i, Wheel((i + j) & 255));
+        }
+        strip.show();
+        delay(wait);
+    }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait)
+{
+    uint16_t i, j;
+
+    for (j = 0; j < 256 * 5; j++)
+    { // 5 cycles of all colors on wheel
+        for (i = 0; i < strip.numPixels(); i++)
+        {
+            strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+        }
+        strip.show();
+        delay(wait);
+    }
+}
+
+uint32_t Wheel(byte WheelPos)
+{
+    WheelPos = 255 - WheelPos;
+    if (WheelPos < 85)
+    {
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    }
+    if (WheelPos < 170)
+    {
+        WheelPos -= 85;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    }
+    WheelPos -= 130;
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
