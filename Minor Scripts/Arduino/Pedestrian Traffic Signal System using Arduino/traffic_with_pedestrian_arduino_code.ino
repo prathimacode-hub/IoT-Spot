d32e5d90b8e66033713dd49ffe2aa276b2e2243a
Sarthak S Kumar <sskworld9742@gmail.com>
2022-04-19 17:12:13 +0530

    Changes in code file

--- Minor Scripts/Arduino/Pedestrian Traffic Signal System using Arduino/traffic_with_pedestrian_arduino_code.ino
+++ Minor Scripts/Arduino/Pedestrian Traffic Signal System using Arduino/traffic_with_pedestrian_arduino_code.ino
@@ -52,3 +52,75 @@ void setup()
   digitalWrite(r4, HIGH);
   digitalWrite(pRed, HIGH);
 }
+
+void loop()
+{
+
+  int crossIn = digitalRead(pIN);
+
+  if (crossIn == HIGH)
+  {
+    WalkCycle();
+  }
+  else
+  {
+    delay(100);
+    digitalWrite(r1, LOW);
+    digitalWrite(g1, HIGH);
+    digitalWrite(r2, HIGH);
+    digitalWrite(r3, HIGH);
+    digitalWrite(r4, HIGH);
+    delay(4000);
+  }
+
+  int crossIn2 = digitalRead(pIN);
+
+  if (crossIn2 == HIGH)
+  {
+    WalkCycle();
+  }
+  else
+  {
+    digitalWrite(g1, LOW);
+
+    digitalWrite(y1, HIGH);
+    digitalWrite(r2, HIGH);
+    digitalWrite(r3, HIGH);
+    digitalWrite(r4, HIGH);
+    delay(2000);
+  }
+
+  int crossIn3 = digitalRead(pIN);
+
+  if (crossIn3 == HIGH)
+  {
+    WalkCycle();
+  }
+  else
+  {
+    digitalWrite(y1, LOW);
+    digitalWrite(r2, LOW);
+
+    digitalWrite(r1, HIGH);
+    digitalWrite(g2, HIGH); // 2nd light
+    digitalWrite(r3, HIGH);
+    digitalWrite(r4, HIGH);
+    delay(4000);
+  }
+
+  int crossIn4 = digitalRead(pIN);
+
+  if (crossIn4 == HIGH)
+  {
+    WalkCycle();
+  }
+  else
+  {
+    digitalWrite(g2, LOW);
+
+    digitalWrite(y2, HIGH);
+    digitalWrite(r1, HIGH);
+    digitalWrite(r3, HIGH);
+    digitalWrite(r4, HIGH);
+    delay(2000);
+  }
