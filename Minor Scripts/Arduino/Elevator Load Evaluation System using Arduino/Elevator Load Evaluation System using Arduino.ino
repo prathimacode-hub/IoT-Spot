e766e2db5bd050d0becf60a472e99925db7b8ad4
Sarthak S Kumar <sskworld9742@gmail.com>
2022-04-25 08:46:02 +0530

    Changes in Code file.

--- Minor Scripts/Arduino/Elevator Load Evaluation System using Arduino/Elevator Load Evaluation System using Arduino.ino
+++ Minor Scripts/Arduino/Elevator Load Evaluation System using Arduino/Elevator Load Evaluation System using Arduino.ino
@@ -6,7 +6,7 @@ int IN2 = 5;
 int DOUT = 3;
 int CLK = 2;
 
-double cf = 18029.57;
+double cf = 18029.57; // Calibration Factor
 
 int Bstart = 6;
 int Bstop = 7;
@@ -17,3 +17,21 @@ float weight;
 HX711 scale;
 
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+
+void setup()
+{
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    lcd.begin(16, 2);
+
+    pinMode(Bstart, INPUT);
+    pinMode(Bstop, INPUT);
+    pinMode(Bstopage, INPUT);
+
+    scale.begin(DOUT, CLK);
+    scale.set_scale(calibration_factor); // This value is obtained by using the SparkFun_HX711_Calibration sketch
+    scale.tare();                        // Assuming there is no weight on the scale at start up, reset the scale to 0
+}
