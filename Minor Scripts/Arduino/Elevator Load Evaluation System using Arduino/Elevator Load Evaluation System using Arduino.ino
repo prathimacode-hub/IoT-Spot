9f756714d3004502db40246c288004f9f7715434
Sarthak S Kumar <sskworld9742@gmail.com>
2022-04-25 08:44:24 +0530

    Changes in Code file

--- Minor Scripts/Arduino/Elevator Load Evaluation System using Arduino/Elevator Load Evaluation System using Arduino.ino
+++ Minor Scripts/Arduino/Elevator Load Evaluation System using Arduino/Elevator Load Evaluation System using Arduino.ino
@@ -1,16 +1,19 @@
-
 #include "HX711.h"
 #include <LiquidCrystal.h>
 
-#define calibration_factor 18029.57 // This value is obtained using the SparkFun_HX711_Calibration sketch
-#define IN1 4
-#define IN2 5
-#define DOUT 3
-#define CLK 2
+int IN1 = 4;
+int IN2 = 5;
+int DOUT = 3;
+int CLK = 2;
 
-#define Bstart 6
-#define Bstop 7
+double cf = 18029.57;
+
+int Bstart = 6;
+int Bstop = 7;
+int Bstopage = 1;
+
 float weight;
-#define Bstopage 1
+
 HX711 scale;
+
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
