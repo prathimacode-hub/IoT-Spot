af74e3d1f07f3e0b16b18a402027b51bfb374397
Ankit Rao <72241207+DragonUncaged@users.noreply.github.com>
2022-05-17 23:25:55 +0530

    Door Bell

---
+++ Arduino/Door Bell/README.md
@@ -0,0 +1,32 @@
+# COVID DOOR BELL
+## AIM:
+To Ring the Bell As Soon As Somebody's Finger Gets Close To The Switch.
+
+## Purpose:
+Automatic DoorBell
+
+## Short Description:
+**Requirements:**
+- Arduino UNO
+- LCD 16X2
+- PIR Sensor
+- Speaker/Door Bell
+(Tinkercad can also be used)
+
+## Workflow:
+- First we initialise Signal PIN, Buzzer Pin.
+- We initiate a Conditional statement For the Buzzer to Ring
+- A Resistor (1 Kohm) is added For the LCD
+
+## Setup instructions:
+- Assemble the circuit as shown below.
+- Upload the code provided [here]()
+- Run to see the project in action!
+------------
+## Output:
+[Simulation Video]()
+![Tinkercad Circuit]()
+------------
+
+## Author:
+[DragonUncaged](https://github.com/dragonuncaged)
---
+++ Arduino/Door Bell/alternate_led_blinking.ino
@@ -0,0 +1,37 @@
+#include<LiquidCrystal.h>
+int signalpin=2;
+int buzzerpin=3;
+int rs=12, en=11, d4=7, d5=6,d6=5,d7=4;
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+void setup()
+{
+  pinMode(signalpin,INPUT);
+  pinMode(buzzerpin,OUTPUT);
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print("DragonUncaged's House");
+  lcd.setCursor(0,1);
+  lcd.print("Get Near to Door");
+  }
+void loop()
+{
+  if(digitalRead(signalpin)==HIGH)
+    {
+    tone(buzzerpin,200);
+     delay(50);
+     tone(buzzerpin,500);
+     delay(50);
+     tone(buzzerpin,900);
+     delay(50);
+     tone(buzzerpin,1000);
+     delay(50);
+     tone(buzzerpin,1300);
+     delay(50);
+     tone(buzzerpin,1800);
+     delay(50);
+     } 
+  else
+    {
+    noTone(buzzerpin);
+    }
+}
