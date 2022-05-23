a25f241e199b7149aede856c9926b9afbf52bffb
Mohamed Ismail Amer <64151841+mohamedismail53@users.noreply.github.com>
2022-05-23 02:58:09 +0200

    Building LCD Driver for interfacing with atmega32

---
+++ Atmel/LCD_Interface_with_atmega32/Code/LCD_Driver/Debug/main.d
@@ -0,0 +1,29 @@
+main.d main.o: .././main.c .././LCD_INT.h .././delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ .././LCD_CONFIG.h .././DIO_INT.h .././std_types.h .././DIO_REG.h
+
+.././LCD_INT.h:
+
+.././delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+.././LCD_CONFIG.h:
+
+.././DIO_INT.h:
+
+.././std_types.h:
+
+.././DIO_REG.h:
---
+++ Atmel/LCD_Interface_with_atmega32/Code/LCD_Driver/std_types.h
@@ -0,0 +1,26 @@
+/*
+ * std_types.h
+ * Description: define standard data types 
+ * Created: 5/21/2022 2:42:56 PM
+ * Author: Mohamed Ismail
+ */ 
+
+
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef unsigned char	uint8;
+typedef unsigned short int uint16;
+typedef unsigned long  int uint32;
+
+typedef signed char	sint8;
+typedef signed short int sint16;
+typedef signed long  int sint32;
+
+typedef float  float32;
+typedef double float64;
+
+
+
+#endif /* STD_TYPES_H_ */
