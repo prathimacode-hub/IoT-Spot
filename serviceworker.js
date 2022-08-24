923675b7a21a215fea16b00b5d7c4d517ad43c09
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2022-08-24 22:10:39 +0530

    PWA

---
+++ serviceworker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "version-1"
+const urlsToCache = ['index.html', 'offline.html']
+
+const self = this
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache')
+                return cache.addAll(urlsToCache);
+            }
+            )
+    )
+}
+)
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+})
+
+self.addEventListener('activate', (event) => {
+    const cacheWhiteList = [];
+    cacheWhiteList.push(CACHE_NAME)
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if (!cacheWhiteList.includes(cacheName)) {
+                    return caches.delete(cacheName)
+                }
+            })
+        ))
+    )
+})
