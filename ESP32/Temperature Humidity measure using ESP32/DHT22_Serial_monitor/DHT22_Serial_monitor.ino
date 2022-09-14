f4c1b74f34b902cc575d6c8a785d1c30f572d188
Adithya S K <adithyaskolavi@gmail.com>
2022-09-14 09:15:56 +0530

    code for DHT22 web server added - Adithya S K

--- ESP32/Temperature Humidity measure using ESP32/DHT22_Serial_monitor/DHT22_Serial_monitor.ino
+++ ESP32/Temperature Humidity measure using ESP32/DHT22_Serial_monitor/DHT22_Serial_monitor.ino
@@ -1,5 +1,3 @@
-// Example testing sketch for various DHT humidity/temperature sensors written by ladyada
-// REQUIRES the following Arduino libraries:
 // - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
 // - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
 
