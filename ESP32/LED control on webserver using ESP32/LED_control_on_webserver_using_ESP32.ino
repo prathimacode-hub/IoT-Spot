3d293085a5655d9c6ecc2232092d0f3ecfc20b13
Adithya S K <adithyaskolavi@gmail.com>
2022-09-19 23:33:45 +0530

    client code added - Adithya S K

--- ESP32/LED control on webserver using ESP32/LED_control_on_webserver_using_ESP32.ino
+++ ESP32/LED control on webserver using ESP32/LED_control_on_webserver_using_ESP32.ino
@@ -46,6 +46,75 @@ void loop(){
   if (client) {                            
     Serial.println("New Client is requesting web page");          
     String current_data_line = "";               
-    
+    while (client.connected()) {            
+      if (client.available()) {             
+        char new_byte = client.read();             
+        Serial.write(new_byte);                    
+        header += new_byte;
+        if (new_byte == '\n') {                    
+         
+          if (current_data_line.length() == 0) 
+          {
+            
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            if (header.indexOf("LED0=ON") != -1) 
+            {
+              Serial.println("GPIO23 LED is ON");
+              LED_ONE_STATE = "on";
+              digitalWrite(GPIO_PIN_NUMBER_22, HIGH);
+            } 
+            if (header.indexOf("LED0=OFF") != -1) 
+            {
+              Serial.println("GPIO23 LED is OFF");
+              LED_ONE_STATE = "off";
+              digitalWrite(GPIO_PIN_NUMBER_22, LOW);
+            } 
+            if (header.indexOf("LED1=ON") != -1)
+            {
+              Serial.println("GPIO23 LED is ON");
+              LED_TWO_STATE = "on";
+              digitalWrite(GPIO_PIN_NUMBER_23, HIGH);
+            }
+            if (header.indexOf("LED1=OFF") != -1) 
+            {
+              Serial.println("GPIO23 LED is OFF");
+              LED_TWO_STATE = "off";
+              digitalWrite(GPIO_PIN_NUMBER_23, LOW);
+            }
+            if (header.indexOf("LED2=ON") != -1) 
+            {
+              Serial.println("GPIO15 LED is ON");
+              LED_THREE_STATE = "on";
+              digitalWrite(GPIO_PIN_NUMBER_15, HIGH);
+            }
+            if(header.indexOf("LED2=OFF") != -1) {
+              Serial.println("GPIO15 LED is OFF");
+              LED_THREE_STATE = "off";
+              digitalWrite(GPIO_PIN_NUMBER_15, LOW);
+            }
+            
+            
+          } 
+          else 
+          { 
+            current_data_line = "";
+          }
+        } 
+        else if (new_byte != '\r') 
+        {  
+          current_data_line += new_byte;     
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
 }
