43ece609b64c53b25fdf58c46eee0cc45cce66c0
Adithya S K <adithyaskolavi@gmail.com>
2022-10-20 03:33:31 +0530

    void set up complete - Adithya S K

--- ESP32/Bluetooth GPIO Control with ESP32/Bluetooth GPIO_Control_with_ESP32.ino
+++ ESP32/Bluetooth GPIO Control with ESP32/Bluetooth GPIO_Control_with_ESP32.ino
@@ -5,3 +5,12 @@ char receivedChar;// received value will be stored as CHAR in this variable
 const char turnON ='ON';
 const char turnOFF ='OFF';
 const int LEDpin = 15;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_Bluetooth"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  Serial.println("To turn ON send: ON");//print on serial monitor  
+  Serial.println("To turn OFF send: OFF"); //print on serial monitor 
+  pinMode(LEDpin, OUTPUT);
+}
