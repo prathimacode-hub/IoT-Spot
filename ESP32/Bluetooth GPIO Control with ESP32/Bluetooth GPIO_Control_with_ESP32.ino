24641c6b3f7a4147d0c7f33fc7a0b6154bc902d0
Adithya S K <adithyaskolavi@gmail.com>
2022-10-20 03:33:07 +0530

    pin set up - Adithya S K

---
+++ ESP32/Bluetooth GPIO Control with ESP32/Bluetooth GPIO_Control_with_ESP32.ino
@@ -0,0 +1,7 @@
+#include "BluetoothSerial.h"
+BluetoothSerial SerialBT;
+char receivedChar;// received value will be stored as CHAR in this variable
+
+const char turnON ='ON';
+const char turnOFF ='OFF';
+const int LEDpin = 15;
