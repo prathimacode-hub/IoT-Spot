9cda683264c017851fca6bdffbd418b00740da4e
Adithya S K <adithyaskolavi@gmail.com>
2022-10-23 23:52:03 +0530

    send data void loop added - Adithya S K

--- ESP32/Firebase Send Read data using ESP32/Firebase_Send_data_using_ESP32.ino
+++ ESP32/Firebase Send Read data using ESP32/Firebase_Send_data_using_ESP32.ino
@@ -67,3 +67,30 @@ void setup(){
   Firebase.reconnectWiFi(true);
 }
 
+void loop(){
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    count++;
+    
+    // Write an Float number on the database path test/float
+    if (Firebase.RTDB.setFloat(&fbdo, "test/float", 0.01 + random(0,100))){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+}
