2093f9ba25c6c02510c3f9ad6e9e81794433ff76
Adithya S K <adithyaskolavi@gmail.com>
2022-10-24 00:00:41 +0530

    folder structure updated - Adithya SK

--- ESP32/Firebase Send Read data using ESP32/Firebase_Read_data_using_ESP32/Firebase_Read_data_using_ESP32.ino
+++ ESP32/Firebase Send Read data using ESP32/Firebase_Read_data_using_ESP32/Firebase_Read_data_using_ESP32.ino
@@ -1,13 +1,3 @@
-/*
-  Rui Santos
-  Complete project details at our blog.
-    - ESP32: https://RandomNerdTutorials.com/esp32-firebase-realtime-database/
-    - ESP8266: https://RandomNerdTutorials.com/esp8266-nodemcu-firebase-realtime-database/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-  Based in the RTDB Basic Example by Firebase-ESP-Client library by mobizt
-  https://github.com/mobizt/Firebase-ESP-Client/blob/main/examples/RTDB/Basic/Basic.ino
-*/
 
 #include <Arduino.h>
 #if defined(ESP32)
